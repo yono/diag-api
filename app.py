3bc1b445aa27b71e909770ee84854f5c9b6827c1
Yusaku ONO <yono.05@gmail.com>
2011-04-24 08:37:52 +0900

    add: seqdiag/actdiag/netdiag

--- app.py
+++ app.py
@@ -4,9 +4,10 @@
 import datetime
 import time
 
-from blockdiag import diagparser
-from blockdiag import blockdiag
-from blockdiag import DiagramDraw
+import blockdiag as bd
+import seqdiag.command as sd
+import actdiag as ad
+import netdiag as nd
 
 from flask import Flask
 from flask import request
@@ -34,15 +35,113 @@ def show_blockdiag():
     unixtime = int(time.mktime(datetime.datetime.now().timetuple()))
     outfile = 'blockdiag%d.png' % (unixtime)
 
-    tree = diagparser.parse(diagparser.tokenize(data))
-    diagram = blockdiag.ScreenNodeBuilder.build(tree)
-    draw = DiagramDraw.DiagramDraw('PNG', diagram, 'static/%s' % (outfile),
+    tree = bd.diagparser.parse(bd.diagparser.tokenize(data))
+    diagram = bd.blockdiag.ScreenNodeBuilder.build(tree)
+    draw = bd.DiagramDraw.DiagramDraw('PNG',diagram,'static/%s' % (outfile),
             font='/Library/Fonts/ヒラギノ明朝 Pro W3.otf', antialias=True)
 
     draw.draw()
     draw.save()
 
-    return render_template('index.html', data=data, outfile=outfile)
+    return render_template('index.html', data=data, outfile=outfile, 
+                           diag="blockdiag")
+
+@app.route("/seqdiag", methods=['POST', 'GET'])
+def show_seqdiag():
+    if 'aaa' in request.form:
+        data = request.form['aaa']
+    else:
+        data = u"""
+{
+  // simple notation
+    browser  -> webserver [label = "GET /index.html"];
+    browser <-- webserver;
+}
+        """;
+
+    unixtime = int(time.mktime(datetime.datetime.now().timetuple()))
+    outfile = 'seqdiag%d.png' % (unixtime)
+
+    tree = sd.diagparser.parse(sd.diagparser.tokenize(data))
+    diagram = sd.ScreenNodeBuilder.build(tree)
+    draw = sd.DiagramDraw('PNG', diagram, 'static/%s' % (outfile),
+            font='/Library/Fonts/ヒラギノ明朝 Pro W3.otf', antialias=True)
+
+    draw.draw()
+    draw.save()
+
+    return render_template('index.html', data=data, outfile=outfile, 
+                           diag="seqdiag")
+
+@app.route("/actdiag", methods=['POST', 'GET'])
+def show_actdiag():
+    if 'aaa' in request.form:
+        data = request.form['aaa']
+    else:
+        data = u"""
+diagram {
+  A -> B -> C;
+  lane you {
+    A; B;
+  }
+  lane me {
+    C;
+  }
+}
+        """;
+
+    unixtime = int(time.mktime(datetime.datetime.now().timetuple()))
+    outfile = 'actdiag%d.png' % (unixtime)
+
+    tree = ad.diagparser.parse(ad.diagparser.tokenize(data))
+    diagram = ad.actdiag.ScreenNodeBuilder.build(tree)
+    draw = ad.DiagramDraw.DiagramDraw('PNG', diagram, 'static/%s' % (outfile),font='/Library/Fonts/ヒラギノ明朝 Pro W3.otf', antialias=True)
+
+    draw.draw()
+    draw.save()
+
+    return render_template('index.html', data=data, outfile=outfile, 
+                           diag="actdiag")
+
+@app.route("/netdiag", methods=['POST', 'GET'])
+def show_netdiag():
+    if 'aaa' in request.form:
+        data = request.form['aaa']
+    else:
+        data = u"""
+diagram {
+  network dmz {
+      address = "210.x.x.x/24"
+
+      web01 [address = "210.x.x.1"];
+      web02 [address = "210.x.x.2"];
+      web03 [address = "210.x.x.3"];
+  }
+  network internal {
+      address = "172.x.x.x/24";
+
+      db01;
+      db02;
+      app01;
+      app02;
+  }
+
+  dmz -- internal
+}
+        """;
+
+    unixtime = int(time.mktime(datetime.datetime.now().timetuple()))
+    outfile = 'netdiag%d.png' % (unixtime)
+
+    tree = nd.diagparser.parse(nd.diagparser.tokenize(data))
+    diagram = nd.netdiag.ScreenNodeBuilder.build(tree)
+    draw = nd.DiagramDraw.DiagramDraw('PNG', diagram, 'static/%s' % (outfile),font='/Library/Fonts/ヒラギノ明朝 Pro W3.otf', antialias=True)
+
+    draw.draw()
+    draw.save()
+
+    return render_template('index.html', data=data, outfile=outfile, 
+                           diag="netdiag")
 
 if __name__ == "__main__":
     app.debug = True
